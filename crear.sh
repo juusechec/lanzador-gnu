c3057e099c947d747555ae44ed23a210296e37a7
Jorge Ulises <naturalmentejorge@gmail.com>
2016-10-03 22:24:09 -0500

    Update crear.sh

--- crear.sh
+++ crear.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo 'El nombre de la aplicación:'
 read nombre
-echo 'La descripción:'
+echo 'Descripción de la aplicación:'
 read descripcion
+echo 'Comando, ruta aplicación [absoluta]:'
+read comando
 echo 'Ruta icono:'
 read icono
-echo 'Comando:'
-read comando
 nombreicono=$(echo "$nombre" | tr '[:upper:]' '[:lower:]' | sed 's/\ /-/g')
 sudo tee /usr/share/applications/$nombreicono.desktop << EOF
 [Desktop Entry]
@@ -18,6 +18,6 @@ Comment=$descripcion
 Icon=$icono
 Exec=$comando
 Terminal=false
-Categories=GTK;Application;AudioVideo;Video;Player;
+Categories=GTK;Application;
 MimeType=application/x-shockwave-flash;
 EOF
