7c97ff84f66cba4fb2b2e023f62fd068401a84de
Jorge Ulises <naturalmentejorge@gmail.com>
2017-10-19 18:36:22 -0500

    Se agrega soporte a enlaces locales

--- crear.sh
+++ crear.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+echo 'Bienvenido al creador de lanzadores para GNOME'
+echo 'Ingrese algunos datos...'
 echo 'El nombre de la aplicación:'
 read nombre
 echo 'Descripción de la aplicación:'
 read descripcion
-echo 'Comando, ruta aplicación [absoluta]:'
+echo 'Comando, ruta de la aplicación [absoluta]:'
 read comando
-echo 'Ruta icono:'
+echo 'Ruta del icono (png,jpg,xpm) [absoluta]:'
 read icono
+# rationale: lowercase
 nombreicono=$(echo "$nombre" | tr '[:upper:]' '[:lower:]' | sed 's/\ /-/g')
-sudo tee /usr/share/applications/$nombreicono.desktop << EOF
+echo '¿Quiere que los demás usuarios accedan al lanzador?: (y/n)'
+read local
+# rationale: lowercase an first letter
+local=$(echo "$local" | tr '[:upper:]' '[:lower:]' | head -c 1)
+if [ "$local" = "y" ]; then
+  APP_DIR='~/.local/share/applications/'
+  SUDO=''
+else
+  APP_DIR='/usr/share/applications/'
+  SUDO='sudo'
+fi
+
+$SUDO tee ${APP_DIR}${nombreicono}.desktop << EOF
 [Desktop Entry]
 Type=Application
 Encoding=UTF-8
